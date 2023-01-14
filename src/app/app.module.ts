d607931ec23e253f478fe1f667be6365f8bd6551
halangbacca <halanbacca@outlook.com>
2023-01-14 14:51:54 -0300

    Cards | ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CaptalizePhilipsPipe } from './pipes/captalize-philips.pipe';
 import { HeaderComponent } from './components/header/header.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CaptalizePhilipsPipe,
-    HeaderComponent
+    HeaderComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
