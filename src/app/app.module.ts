5458d36f0c8f5c7e86470e43f85d50874b81a8d0
halangbacca <halanbacca@outlook.com>
2023-01-14 13:04:05 -0300

    Pipe | Camel Case

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CaptalizePhilipsPipe } from './pipes/captalize-philips.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaptalizePhilipsPipe
   ],
   imports: [
     BrowserModule,
